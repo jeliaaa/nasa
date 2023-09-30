3805c793f309a637af012aa68c3946acd8d166eb
jeliaaa <julia.aleqsandre@gmail.com>
2023-09-30 14:57:10 +0400

    last

--- src/pages/crime/Crime.jsx
+++ src/pages/crime/Crime.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import './crime.scss'
+import { Table } from 'react-bootstrap'
 const Crime = () => {
   return (
-    <div className='crimeMain'>
+    <div>
+      <div className='crimeMain'>
         <p>ბრაკონიერები არიან ადამიანები ვინც აზიანებს ბუნებას და ანადგურებს გარემოს, ამ საზოგადოებაში ასეთი ადამიანებს არ მივესალმებით!</p>
         <button>შეგვატყობინეთ</button>
+      </div>
+      <div className='crime_stats mt-5'>
+        <Table striped>
+          <thead>
+            <tr>
+              <td>ადგილი</td>
+              <td>დრო</td>
+              <td>ძაბვა</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>ადგილი</td>
+              <td>10:43</td>
+              <td>128ვ</td>
+            </tr>
+            <tr>
+              <td>ადგილი</td>
+              <td>10:43</td>
+              <td>128ვ</td>
+            </tr>
+            <tr>
+              <td>ადგილი</td>
+              <td>10:43</td>
+              <td>128ვ</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </div>
   )
 }
